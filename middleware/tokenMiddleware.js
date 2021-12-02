35c239b8addee65f1067768737945b65d7c61480
Dawid <dawid.jm@interia.pl>
2021-12-02 21:51:10 +0100

    Token auth in swagger done

--- middleware/tokenMiddleware.js
+++ middleware/tokenMiddleware.js
@@ -7,7 +7,9 @@ const authenticate = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (token === null) return res.sendStatus(401);
+    if (token === null) {
+        return res.sendStatus(401);
+    };
 
     jwt.verify(token, env.TOKEN_SECRET, (err, usr) => {
         if (err) return res.sendStatus(403);
